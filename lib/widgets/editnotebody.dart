a940cef249dea6bc33b7099aaaaf6455b7a63d04
selim <selimexiled@gmail.com>
2023-08-25 17:51:22 +0300

    all ui

--- lib/widgets/editnotebody.dart
+++ lib/widgets/editnotebody.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:selimnotes/widgets/customTextField.dart';
 import 'package:selimnotes/widgets/custom_appbar.dart';
 
 class EditNoteBody extends StatelessWidget {
@@ -10,7 +11,11 @@ class EditNoteBody extends StatelessWidget {
       padding: const EdgeInsets.all(24.0),
       child: Column(
         children: [
-          CustomAppbar(title: 'Edit Note',icon: Icons.check,)
+          CustomAppbar(title: 'Edit Note',icon: Icons.check,),
+          SizedBox(height: 32,),
+          CustomTextField(hintText: 'Title'),
+          SizedBox(height: 16,),
+          CustomTextField(hintText: 'Content',maxLines: 5,)
         ],
       ),
     );
