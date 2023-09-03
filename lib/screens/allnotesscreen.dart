821a517676092faf9e66d56a0848a4c465ccaac1
selim <selimexiled@gmail.com>
2023-09-03 07:00:53 +0300

    create fetch notes method

--- lib/screens/allnotesscreen.dart
+++ lib/screens/allnotesscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:selimnotes/cubits/notes_cubit/notes_cubit.dart';
 import '../widgets/add-note-bottomsheet.dart';
 import '../widgets/allnotesbody.dart';
 
@@ -9,20 +10,23 @@ class AllNotesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.greenAccent,
-            onPressed: () {
-              showModalBottomSheet(
-                isScrollControlled: true,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16)),
-                  context: context,
-                  builder: (context) => AddNoteBottomSheet());
-            },
-            child: Icon(Icons.add),
-          ),
-          body: const AllNotesBody()),
+      child: BlocProvider(
+        create: (context) => NotesCubit(),
+        child: Scaffold(
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.greenAccent,
+              onPressed: () {
+                showModalBottomSheet(
+                    isScrollControlled: true,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    context: context,
+                    builder: (context) => AddNoteBottomSheet());
+              },
+              child: Icon(Icons.add),
+            ),
+            body: const AllNotesBody()),
+      ),
     );
   }
 }
